50b856a81e58b70b64dc7e45e502c72e158dbc91
mahakamal-e <mahakamal09@gmail.com>
2024-01-19 14:17:47 -0800

    fix error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
+	size_t i, j;
 	int temp;
 
 	if (array == NULL || size == 0)
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - j - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
