bf5459e8db0610f91604cc51ea8706840af57218
mahakamal-e <mahakamal09@gmail.com>
2024-01-19 16:53:43 -0800

    betty fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,16 +10,16 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *currNode, *prevNode;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	
+
 	currNode = (*list)->next;
-	
+
 	while (currNode)
 	{
 		prevNode = currNode->prev;
-		
+
 		while (prevNode != NULL && prevNode->n > currNode->n)
 		{
 			if (prevNode->prev != NULL)
@@ -28,13 +28,13 @@ void insertion_sort_list(listint_t **list)
 			prevNode->prev = currNode;
 			prevNode->next = currNode->next;
 			currNode->next = prevNode;
-			
+
 			if (prevNode->next != NULL)
 				prevNode->next->prev = prevNode;
-			
+
 			if (prevNode == *list)
 				*list = currNode;
-			
+
 			print_list(*list);
 			prevNode = currNode->prev;
 		}
--- sort.h
+++ sort.h
@@ -26,6 +26,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap_nodes(
-                listint_t **list, listint_t *currNode, listint_t *pervNode);
+
+
 #endif
