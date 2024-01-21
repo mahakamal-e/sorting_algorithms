17038d7400c78305cd6a625ddaed90420bf39580
mahakamal-e <mahakamal09@gmail.com>
2024-01-21 09:04:29 -0800

    done

---
+++ 103-merge_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+/**
+ * merge_sort - a function that sorts an array of integers
+ * in ascending order using the Merge sort algorithm.
+ *
+ * @array: pointer to array to sort.
+ * @size: the size of input array.
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *tempArray;
+	size_t = mid
+	if (size > 1)
+	{
+
